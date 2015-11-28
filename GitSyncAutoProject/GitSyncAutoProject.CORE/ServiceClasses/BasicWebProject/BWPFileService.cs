77670c8dce7bf4f8897a924b4672da9bfd1386cb
jeffward01 <jeffwardcoding@gmail.com>
2015-11-28 15:27:00 -0800

    added file creation and writing complete

--- GitSyncAutoProject/GitSyncAutoProject.CORE/ServiceClasses/BasicWebProject/BWPFileService.cs
+++ GitSyncAutoProject/GitSyncAutoProject.CORE/ServiceClasses/BasicWebProject/BWPFileService.cs
@@ -8,7 +8,31 @@ namespace GitSyncAutoProject.CORE.ServiceClasses.BasicWebProject
 {
     public class BWPFileService
     {
+        public static string HTMLStringBuilder()
+        {
+            return @"
+<!DOCTYPE html>
+<html lang = 'en'>
+    <head>
+        <meta charset = 'utf-8'>
+        <link rel = 'stylesheet' media = 'screen' href = 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.min.css'>
+        <link rel = 'stylesheet' href = 'css/main.css'>        
+        <title> Your Title </title>         
+    </head>   
+    <body>
+    
 
+
+
+            
+    <script src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js' ></script>
+    <script src = 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js' ></script>
+    <script src = 'js/script.js'></script>
+                   
+    </body>
+                   
+ </html> ";
+        }
 
         
 
